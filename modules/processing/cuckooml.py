8c7ca7050f9c5bb3af3055a06d5bb46efaec88d7
Cindy Xue <cindyxue1405@gmail.com>
2019-07-24 13:46:08 -0700

    calculate similarities

--- modules/processing/cuckooml.py
+++ modules/processing/cuckooml.py
@@ -1377,6 +1377,7 @@ class ML(object):
 
         return target_counter_dict
 
+
     # Print features that exist
     def count_and_print_features(self, classfier=None, target_features=None, label=-1, do_print=False):
         if classfier is None:
@@ -1412,6 +1413,29 @@ class ML(object):
             
         return target_counter_dict
 
+
+    # Calculate avr similarities for each key values
+    def calc_similarity_self(self, count_features_arr, do_print=False):
+        res = {}
+        for item in count_features_arr:
+            cur_vector = count_features_arr[item]
+            similarities = []
+            for other_item in count_features_arr:
+                if item == other_item:
+                    continue
+                else:
+                    vec1 = np.array([count_features_arr[item]])
+                    vec2 = np.array([count_features_arr[other_item]])
+                    cur_sim = cosine_similarity(vec1, vec2)
+                    similarities.append(cur_sim)
+        #             print cur_sim
+            avr_sim = reduce(lambda x, y: x + y, similarities) / len(similarities)
+            res[item] = avr_sim
+
+        if do_print:
+            print(res)
+        
+        return res        
 
 class Loader(object):
     """Loads instances for analysis and give possibility to extract properties
